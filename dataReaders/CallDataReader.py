45cd58c451fb6e6e1002762bae5cb59a2b171c50
Hruthik Ketepalli <h.ketepalli@smartnumbers.com>
2023-02-17 16:19:59 +0000

    Implemented read_data method for call data reader

--- dataReaders/CallDataReader.py
+++ dataReaders/CallDataReader.py
@@ -1,11 +1,30 @@
+import json
+from datetime import datetime
 from typing import List
 
 from models.Call import Call
+from models.CallBuilder import CallBuilder
 from .DataReader import DataReader
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
 class CallDataReader(DataReader):
 
     @staticmethod
     def read_data(filename: str) -> List[Call]:
-        pass
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            calls = []
+
+            for call in data['data']:
+                calls.append(CallBuilder().call_type(call['type'])
+                             .in_red_list(call['attributes']['redList'])
+                             .in_green_list(call['attributes']['greenList']) \
+                             .phone_number(call['attributes']['number']) \
+                             .call_risk_score(call['attributes']['riskScore']) \
+                             .call_datetime(datetime.strptime(call['attributes']['date'], DATETIME_FORMAT)) \
+                             .call_id(call['id']) \
+                             .build())
+
+            return calls
