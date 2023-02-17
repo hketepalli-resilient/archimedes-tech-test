76e7abc711e85f354c5b1c380861e84ec017ae0b
Hruthik Ketepalli <h.ketepalli@smartnumbers.com>
2023-02-17 16:59:51 +0000

    Implemented read_data method for operator data reader

--- dataReaders/OperatorDataReader.py
+++ dataReaders/OperatorDataReader.py
@@ -1,6 +1,8 @@
+import json
 from typing import Dict
 
 from models.Operator import Operator
+from models.OperatorBuilder import OperatorBuilder
 from .DataReader import DataReader
 
 
@@ -8,5 +10,19 @@ class OperatorDataReader(DataReader):
 
     # Returns a dict[operator prefix, operator]
     @staticmethod
-    def read_data(filename: str) -> Dict[str, Operator]:
-        pass
+    def read_data(filename: str) -> Dict[int, Operator]:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            operators = {}
+
+            for operator in data['data']:
+                prefix = int(operator['attributes']['prefix'])
+
+                operators[prefix] = OperatorBuilder() \
+                    .operator_name(operator['attributes']['operator']) \
+                    .operator_prefix(prefix) \
+                    .operator_type(operator['type']) \
+                    .operator_id(operator['id']) \
+                    .build()
+
+            return operators
--- tests/dataReaders/TestOperatorDataReader.py
+++ tests/dataReaders/TestOperatorDataReader.py
@@ -1,9 +1,37 @@
 import unittest
+from typing import Dict
+
+from dataReaders.OperatorDataReader import OperatorDataReader
+from models.Operator import Operator
+from models.OperatorBuilder import OperatorBuilder
+
+
+def get_expected_operators() -> Dict[int, Operator]:
+    operator1 = OperatorBuilder() \
+        .operator_name('Vodafone') \
+        .operator_prefix(1000) \
+        .operator_type('operator') \
+        .operator_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b') \
+        .build()
+
+    operator2 = OperatorBuilder() \
+        .operator_name('EE') \
+        .operator_prefix(2000) \
+        .operator_type('operator') \
+        .operator_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de') \
+        .build()
+
+    return {1000: operator1, 2000: operator2}
 
 
 class TestOperatorDataReader(unittest.TestCase):
     def test_read_file(self):
-        pass
+        expected_operators = get_expected_operators()
+
+        operators = OperatorDataReader.read_data('../resources/operators.json')
+
+        self.assertEqual(len(operators), 2)
+        self.assertEqual(operators, expected_operators)
 
 
 if __name__ == '__main__':
