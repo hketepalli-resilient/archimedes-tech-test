8b8287d07b6862e650f871dfdcd161a80b2ac441
Hruthik Ketepalli <h.ketepalli@smartnumbers.com>
2023-02-18 15:19:44 +0000

    Added call summary fixture

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -1,34 +1,56 @@
 from datetime import datetime
 
 from models.CallBuilder import CallBuilder
+from models.CallSummaryBuilder import CallSummaryBuilder
 from models.OperatorBuilder import OperatorBuilder
 
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-CALLS = [CallBuilder().in_green_list() \
-             .phone_number('+44123456789') \
-             .risk_score(0.431513435443) \
-             .datetime(datetime.strptime('2020-10-12T07:20:50.52Z', DATETIME_FORMAT)) \
-             .call_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b') \
-             .build(),
-         CallBuilder().in_red_list() \
-             .phone_number('+44123456789') \
-             .risk_score(0.123444) \
-             .datetime(datetime.strptime('2019-10-12T07:20:50.52Z', DATETIME_FORMAT)) \
-             .call_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de') \
-             .build()
-         ]
+CALLS = [
+    CallBuilder()
+    .in_green_list()
+    .phone_number('+44123456789')
+    .risk_score(0.431513435443)
+    .datetime(datetime.strptime('2020-10-12T07:20:50.52Z', DATETIME_FORMAT))
+    .call_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b')
+    .build(),
+    CallBuilder()
+    .in_red_list()
+    .phone_number('+44123456789')
+    .risk_score(0.123444)
+    .datetime(datetime.strptime('2019-10-12T07:20:50.52Z', DATETIME_FORMAT))
+    .call_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de')
+    .build()
+]
 
 OPERATORS = {
     1000: OperatorBuilder()
-    .name('Vodafone') \
-        .prefix(1000) \
-        .type('operator') \
-        .operator_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b') \
-        .build(),
-    2000: OperatorBuilder() \
-        .name('EE') \
-        .prefix(2000) \
-        .type('operator') \
-        .operator_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de') \
-        .build()}
+    .name('Vodafone')
+    .prefix(1000)
+    .type('operator')
+    .operator_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b')
+    .build(),
+    2000: OperatorBuilder()
+    .name('EE')
+    .prefix(2000)
+    .type('operator')
+    .operator_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de')
+    .build()
+}
+
+CALL_SUMMARY = [
+    CallSummaryBuilder()
+    .call_id('2c4fae60-cf43-4f27-869e-a9ed8b0ca25b')
+    .datetime(datetime.strptime('2020-10-12T07:20:50.52Z', DATETIME_FORMAT))
+    .phone_number('+44123456789')
+    .operator_name('Vodafone')
+    .risk_score(0)
+    .build(),
+    CallSummaryBuilder()
+    .call_id('8f1b1354-26d2-4e16-9582-9156a0d9a5de')
+    .datetime(datetime.strptime('2019-10-12T07:20:50.52Z', DATETIME_FORMAT))
+    .phone_number('+44123456789')
+    .operator_name('EE')
+    .risk_score(1.0)
+    .build()
+]
