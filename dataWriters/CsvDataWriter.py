42e96ede319528c0dbeea7b7a87fac69338c497e
Hruthik Ketepalli <h.ketepalli@smartnumbers.com>
2023-02-18 17:15:48 +0000

    Implemented csv writer

--- dataWriters/CsvDataWriter.py
+++ dataWriters/CsvDataWriter.py
@@ -1,11 +1,26 @@
+from datetime import datetime
 from typing import List
 
 from models.CallSummary import CallSummary
 from .DataWriter import DataWriter
 
+HEADER_LINE = 'id,date,number,operator,riskScore\n'
+
 
 class CsvDataWriter(DataWriter):
 
     @staticmethod
-    def write(data: List[CallSummary]):
-        pass
+    def write(calls_summaries: List[CallSummary]):
+        with open('calls_summaries.txt', 'w') as f:
+            f.write(HEADER_LINE)
+
+            for call_summary in calls_summaries:
+                f.write(CsvDataWriter.get_csv_string(call_summary) + '\n')
+
+    @staticmethod
+    def get_csv_string(data: CallSummary):
+        return f"{data.call_id},{CsvDataWriter.get_date(data.datetime)},{data.number},{data.operator},{data.risk_score}"
+
+    @staticmethod
+    def get_date(date_time: datetime):
+        return date_time.strftime("%Y-%m-%d")
--- tests/dataWriters/TestCsvDataWriter.py
+++ tests/dataWriters/TestCsvDataWriter.py
@@ -1,9 +1,24 @@
 import unittest
 
+from dataWriters.CsvDataWriter import CsvDataWriter
+from tests.fixtures.fixtures import CALLS_SUMMARIES
+
 
 class MyTestCase(unittest.TestCase):
     def test_write(self):
-        self.assertEqual(True, True)
+        CsvDataWriter.write(CALLS_SUMMARIES)
+
+        with open('calls_summaries.txt', 'r') as f:
+            lines = 0
+            for line in f:
+                lines += 1
+
+            self.assertEqual(lines, 3)
+
+    def test_get_date(self):
+        expected_string = '2c4fae60-cf43-4f27-869e-a9ed8b0ca25b,2020-10-12,+44123456789,Vodafone,0.0'
+
+        self.assertEqual(CsvDataWriter.get_csv_string(CALLS_SUMMARIES[0]), expected_string)
 
 
 if __name__ == '__main__':
