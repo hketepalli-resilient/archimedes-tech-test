a2703a31347ece8c4be09d47e25adf14f10c3d7e
Hruthik Ketepalli <h.ketepalli@smartnumbers.com>
2023-02-24 18:49:47 +0000

    Used "parameterized" to paramerise the unit tests

--- tests/data_enrichers/TestDefaultDataEnricher.py
+++ tests/data_enrichers/TestDefaultDataEnricher.py
@@ -1,5 +1,7 @@
 import unittest
 
+from parameterized import parameterized
+
 from data_enrichers.DefaultDataEnricher import DefaultDataEnricher
 from tests.fixtures.fixtures import CALLS, OPERATORS, CALLS_SUMMARIES
 
@@ -12,31 +14,29 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(len(combined_data), 2)
         self.assertEqual(combined_data, CALLS_SUMMARIES)
 
-    def test_get_prefix_range(self):
-        phone_number = '+442143999888'
+    @parameterized.expand([
+        ('+442143999888', 2000),
+        ('+445943999888', 5000),
+        ('Withheld', -1),
+    ])
+    def test_get_prefix_range(self, phone_number: str, expected_outcome: int):
+        self.assertEqual(DefaultDataEnricher.get_prefix_range(phone_number), expected_outcome)
 
-        self.assertEqual(DefaultDataEnricher.get_prefix_range(phone_number), 2000)
-
-        phone_number = 'Withheld'
-
-        self.assertEqual(DefaultDataEnricher.get_prefix_range(phone_number), -1)
-
-    def test_get_risk_score(self):
-        param_list = [
-            (0.42, False, False, 0.4),
-            (0.45, False, False, 0.5),
-            (0.42, True, False, 0.0),
-            (0.42, False, True, 1.0),
-            (0.42, True, True, 0.0),
-        ]
-
-        for risk_sore, in_green_list, in_red_list, expected in param_list:
-            with self.subTest():
-                self.assertEqual(
-                    DefaultDataEnricher.get_risk_score(risk_sore,
-                                                       in_green_list=in_green_list,
-                                                       in_red_list=in_red_list),
-                    expected)
+    @parameterized.expand([
+        (0.42, False, False, 0.4),
+        (0.45, False, False, 0.5),
+        (0.42, True, False, 0.0),
+        (0.42, False, True, 1.0),
+        (0.42, True, True, 0.0),
+    ])
+    def test_get_risk_score(self, risk_sore: float, in_green_list: bool, in_red_list: bool, expected_outcome: float):
+        # For some reason, running this test individually fails so when testing run the entire file
+        self.assertEqual(DefaultDataEnricher.get_risk_score(
+            risk_sore,
+            in_green_list=in_green_list,
+            in_red_list=in_red_list
+        ),
+            expected_outcome)
 
 
 if __name__ == '__main__':
